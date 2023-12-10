5e87a39b1481ee2afa269580790540fd4bd34946
IvanaSpeziale <139582321+IvanaSpeziale@users.noreply.github.com>
2023-12-10 16:36:41 +0100

    Fix duplication issue

--- src/slice/users.slice.test.tsx
+++ src/slice/users.slice.test.tsx
@@ -5,16 +5,20 @@ import { loginThunk, registerThunk } from './users.thunk';
 import { ApiRepoUsers } from '../services/api.repo.users';
 
 describe('Given userSlice', () => {
+  let initialState: UsersState;
+
+  beforeEach(() => {
+    initialState = {
+      loggedUser: {} as User,
+      loginLoadState: 'idle',
+      token: 'someToken',
+      registrationStatus: 'registered',
+    };
+  });
+
   describe('When we call slice with logout action', () => {
     test('then it should set loggedUser to null and token to null', () => {
-      const currentState: UsersState = {
-        loggedUser: {} as User,
-        loginLoadState: 'idle',
-        token: 'someToken',
-        registrationStatus: 'registered',
-      };
-
-      const newState = usersSlice(currentState, logout());
+      const newState = usersSlice(initialState, logout());
 
       expect(newState.loggedUser).toBe(null);
       expect(newState.token).toBe(null);
@@ -23,15 +27,8 @@ describe('Given userSlice', () => {
 
   describe('When we call slice with setToken action', () => {
     test('then it should set token to the provided value', () => {
-      const currentState: UsersState = {
-        loggedUser: {} as User,
-        loginLoadState: 'idle',
-        token: '',
-        registrationStatus: 'registered',
-      };
-
       const newToken = 'newToken';
-      const newState = usersSlice(currentState, setToken(newToken));
+      const newState = usersSlice(initialState, setToken(newToken));
 
       expect(newState.token).toBe(newToken);
     });
@@ -39,14 +36,7 @@ describe('Given userSlice', () => {
 
   describe('When we call slice with clearToken action', () => {
     test('then it should set token to null', () => {
-      const currentState: UsersState = {
-        loggedUser: {} as User,
-        loginLoadState: 'idle',
-        token: 'someToken',
-        registrationStatus: 'registered',
-      };
-
-      const newState = usersSlice(currentState, clearToken());
+      const newState = usersSlice(initialState, clearToken());
 
       expect(newState.token).toBe(null);
     });
@@ -54,16 +44,8 @@ describe('Given userSlice', () => {
 
   describe('When we call slice and login is pending', () => {
     test('then it should set loginLoadState to logging', () => {
-      const currentState: UsersState = {
-        loggedUser: {} as User,
-        loginLoadState: 'idle',
-        token: 'someToken',
-        registrationStatus: 'registered',
-      };
-
       const action = loginThunk.pending;
-
-      const newState = usersSlice(currentState, action);
+      const newState = usersSlice(initialState, action);
 
       expect(newState.loginLoadState).toBe('logging');
     });
@@ -71,36 +53,22 @@ describe('Given userSlice', () => {
 
   describe('When we call slice and login is rejected', () => {
     test('then it should set loggedUser to null and token to null', () => {
-      const currentState: UsersState = {
-        loggedUser: {} as User,
-        loginLoadState: 'idle',
-        token: 'someToken',
-        registrationStatus: 'registered',
-      };
-
-      // Simular la acción loginThunk.rejected
       const error = new Error('Simulated error');
       const action = loginThunk.rejected(error, '', {
         loginUser: {} as LoginUser,
         repo: {} as ApiRepoUsers,
       });
 
-      const newState = usersSlice(currentState, action);
+      const newState = usersSlice(initialState, action);
 
       expect(newState.loggedUser).toBe(null);
       expect(newState.token).toBe(null);
       expect(newState.loginLoadState).toBe('error');
     });
   });
+
   describe('When we call slice and login is fulfilled', () => {
     test('then it should set loggedUser and token based on the action payload', () => {
-      const currentState: UsersState = {
-        loggedUser: null,
-        loginLoadState: 'idle',
-        token: '',
-        registrationStatus: 'registered',
-      };
-
       const user = {} as User;
       const token = 'someToken';
       const action = loginThunk.fulfilled({ user, token }, '', {
@@ -108,41 +76,27 @@ describe('Given userSlice', () => {
         repo: {} as ApiRepoUsers,
       });
 
-      const newState = usersSlice(currentState, action);
+      const newState = usersSlice(initialState, action);
 
       expect(newState.loggedUser).toEqual(user);
       expect(newState.token).toBe(token);
       expect(newState.loginLoadState).toBe('idle');
     });
   });
+
   describe('When we call slice with logout action', () => {
     test('then it should set loggedUser to null and token to null', () => {
-      const currentState: UsersState = {
-        loggedUser: {} as User,
-        loginLoadState: 'idle',
-        token: 'someToken',
-        registrationStatus: 'registered',
-      };
-
-      const newState = usersSlice(currentState, logout());
+      const newState = usersSlice(initialState, logout());
 
       expect(newState.loggedUser).toBe(null);
       expect(newState.token).toBe(null);
     });
   });
+
   describe('When we call slice and register is fulfilled', () => {
     test('then it should update registration status', () => {
-      const currentState: UsersState = {
-        loggedUser: {} as User,
-        loginLoadState: 'idle',
-        token: 'someToken',
-        registrationStatus: 'registered',
-      };
-
-      // Simular la acción registerThunk.fulfilled
       const action = registerThunk.fulfilled;
-
-      const newState = usersSlice(currentState, action);
+      const newState = usersSlice(initialState, action);
 
       expect(newState.registrationStatus).toBe('registered');
     });
