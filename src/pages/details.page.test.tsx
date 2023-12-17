d552fbbfc466dab2014a32caa7703951b3a5caaf
IvanaSpeziale <139582321+IvanaSpeziale@users.noreply.github.com>
2023-12-17 17:47:02 +0100

    Add details page test

--- src/pages/details.page.test.tsx
+++ src/pages/details.page.test.tsx
@@ -1,11 +1,16 @@
 import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { DetailsPage } from '../pages/details.page';
 import { configureStore } from '@reduxjs/toolkit';
 import recipesReducer from '../slice/recipes.slice';
 import { Recipe } from '../entities/recipe';
 import '@testing-library/jest-dom';
+
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useSelector: jest.fn(),
+}));
 
 const store = configureStore({
   reducer: {
@@ -24,13 +29,19 @@ const store = configureStore({
 });
 
 test('should render DetailsPage with recipe details', () => {
+  (useSelector as jest.Mock).mockImplementation(() => ({
+    recipes: [{ id: '1', recipeName: 'Test Recipe' } as unknown as Recipe],
+    currentRecipe: {
+      id: '1',
+      recipeName: 'Test Recipe',
+      img: { url: 'test-url' },
+    } as unknown as Recipe,
+  }));
   const { getByText } = render(
     <Provider store={store}>
       <MemoryRouter initialEntries={['/details/1']}>
         <Routes>
-          <Route path="/details/:id">
-            <DetailsPage />
-          </Route>
+          <Route path="/details/:id" element={<DetailsPage />} />
         </Routes>
       </MemoryRouter>
     </Provider>
